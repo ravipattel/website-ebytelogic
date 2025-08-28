15b522f5e3717d2895a4805bc8cce787ce00e876
ravipatel <ravi@ebytelogic.com>
2025-08-28 13:26:19 +0530

    fix chat bot

--- src/components/tawk.tsx
+++ src/components/tawk.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const Tawk = () => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/688b5f9bfb0a7d192640349a/1j1g6quda";
+    script.src = "https://embed.tawk.to/6888b575a7e9db192a60e65b/1j1b09lf0";
     script.async = true;
     script.charset = "UTF-8";
     script.setAttribute("crossorigin", "*");
