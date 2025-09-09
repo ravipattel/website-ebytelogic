8daa875b674581ca1a692194d9efa3193445f672
harshil-bodara <developer.harshilbodara@gmail.com>
2025-09-09 11:34:56 +0530

    Add contact-us in database

---
+++ utils /toast.ts
@@ -0,0 +1,15 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const SuccessToast = (message) => {
+  toast.success(message, {
+    position: "top-right", // You can change to "bottom-right", "top-center", etc.
+    autoClose: 3000,       // Time in ms before it disappears
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",   
+  });
+};
