cf6f63a0d1431b49edf52ffd2b6482da29cf24af
Ravi-Patel <ravi@ebytelogic.com>
2026-01-09 12:33:17 +0530

    changed title to h1 from h2 in service-detail-page

--- src/views/services/[id]/ServiceDetailsInfo.tsx
+++ src/views/services/[id]/ServiceDetailsInfo.tsx
@@ -626,7 +626,7 @@ const ServiceDetailsInfo = ({ meta }) => {
                     <div className='absolute bg-[#0e191eb3] top-0 size-full z-0'></div>
                     <div className='max-w-[1400px] mx-auto px-4 sm:px-6 relative z-10 space-y-4'>
                         <p className='text-sm md:text-lg text-white flex flex-wrap items-center gap-4 justify-center'><Link href={'/'}>Home</Link>  <RiArrowRightSLine className='text-primary text-2xl' /> <Link href={'/services'}>Services</Link>  <RiArrowRightSLine className='text-primary text-2xl' /> {service.pathText}</p>
-                        <h2 className='text-2xl md:text-5xl xl:text-[50px] font-normal text-white leading-tight text-center pb-4'>{service.category}</h2>
+                        <h1 className='text-2xl md:text-5xl xl:text-[50px] font-normal text-white leading-tight text-center pb-4'>{service.category}</h1>
                         <p className='text-sm md:text-lg text-white flex items-center gap-4 justify-center text-center'>{service.tagLine}</p>
                     </div>
                 </section>
