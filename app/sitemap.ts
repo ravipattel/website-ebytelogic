ec4e295876204f9829b445a032e0515f6869e643
Ravi-Patel <ravi@ebytelogic.com>
2026-02-18 15:58:33 +0530

    updated sitemap file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { getAllBlogs } from "@/content/blogsMetaData"
+import { getBlogs } from "@/src/store/getBlogs";
 import { getAllCaseStudies } from "@/content/casestudyMetaData";
 import { getAllIndustries } from "@/content/industryMetaData";
 import { getAllServices } from "@/content/serviceMetaData";
@@ -7,14 +7,14 @@ import { getAllServices } from "@/content/serviceMetaData";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const base = "https://www.ebytelogic.com";
   const staticUrls = ["/", "/about", "/blogs", "/faqs", "/services", "/industries", "/case-study", "/contact-us"];
-  const blogs = (await getAllBlogs()) ?? [];
+  const blogs = (await getBlogs()) ?? [];
   const casestudy = (await getAllCaseStudies()) ?? [];
   const industries = (await getAllIndustries()) ?? [];
   const services = (await getAllServices()) ?? [];
 
   return [
     ...staticUrls.map((path) => ({ url: `${base}${path}`, changefreq: "weekly" })),
-    ...blogs.map((b) => ({ url: `${base}/blogs/${b.id}`, changefreq: "monthly" })),
+    ...blogs.map((b) => ({ url: `${base}/blogs/${b.slug}`, changefreq: "monthly" })),
     ...casestudy.map((c) => ({ url: `${base}/case-study/${c.id}`, changefreq: "monthly" })),
     ...industries.map((i) => ({ url: `${base}/industries/${i.id}`, changefreq: "monthly" })),
     ...services.map((s) => ({ url: `${base}/services/${s.id}`, changefreq: "monthly" })),
