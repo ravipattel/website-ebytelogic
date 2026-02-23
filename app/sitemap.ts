5b10e59ffe3f5d41a70ead34d3c3216dab2c8601
Ravi-Patel <ravi@ebytelogic.com>
2026-02-23 09:54:18 +0530

    updated sitemap file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,17 +6,28 @@ import { getAllServices } from "@/content/serviceMetaData";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const base = "https://www.ebytelogic.com";
-  const staticUrls = ["/", "/about", "/blogs", "/faqs", "/services", "/industries", "/case-study", "/contact-us"];
+
+  const staticUrls = [
+    { path: "/", priority: 1, changefreq: "weekly", lastmod: new Date() },
+    { path: "/about", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/blogs", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/faqs", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/services", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/industries", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/case-study", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+    { path: "/contact-us", priority: 0.8, changefreq: "weekly", lastmod: new Date() },
+  ];
+
   const blogs = (await getBlogs()) ?? [];
   const casestudy = (await getAllCaseStudies()) ?? [];
   const industries = (await getAllIndustries()) ?? [];
   const services = (await getAllServices()) ?? [];
 
   return [
-    ...staticUrls.map((path) => ({ url: `${base}${path}`, changefreq: "weekly" })),
-    ...blogs.map((b) => ({ url: `${base}/blogs/${b.slug}`, changefreq: "monthly" })),
-    ...casestudy.map((c) => ({ url: `${base}/case-study/${c.id}`, changefreq: "monthly" })),
-    ...industries.map((i) => ({ url: `${base}/industries/${i.id}`, changefreq: "monthly" })),
-    ...services.map((s) => ({ url: `${base}/services/${s.id}`, changefreq: "monthly" })),
-  ];
+    ...staticUrls.map((page) => ({ url: `${base}${page.path}`, changeFrequency: page.changefreq as any, priority: page.priority, lastModified: page.lastmod })),
+    ...blogs.map((b) => ({ url: `${base}/blogs/${b.slug}`, changeFrequency: "weekly", priority: 0.8, lastModified: new Date()  })),
+    ...services.map((s) => ({ url: `${base}/services/${s.id}`, changeFrequency: "weekly", priority: 0.8, lastModified: new Date() })),
+    ...casestudy.map((c) => ({ url: `${base}/case-study/${c.id}`, changeFrequency: "weekly", priority: 0.8, lastModified: new Date() })),
+    ...industries.map((i) => ({ url: `${base}/industries/${i.id}`, changeFrequency: "weekly", priority: 0.8, lastModified: new Date() })),
+    ];
 }
